bd89150f5a09372cd7df1259b33604760a2c73bd
Giovanni-Shiroma <giovanni.shiroma04@gmail.com>
2025-11-15 21:04:37 -0300

    first commit

---
+++ src/br/com/minhasmusicas/principal/Principal.java
@@ -0,0 +1,38 @@
+package br.com.minhasmusicas.principal;
+
+import br.com.minhasmusicas.modelos.MinhasPreferidas;
+import br.com.minhasmusicas.modelos.Musica;
+import br.com.minhasmusicas.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Favorita");
+        minhaMusica.setCantor("Yunk Vino");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+
+        meuPodcast.setTitulo("podpah de verão");
+        meuPodcast.setApresentador("Igão e Mítico");
+
+        for (int i = 0; i < 200; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 400; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+    }
+}
